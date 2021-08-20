8fd0a642cdb2640f5f711713ef9979ebce251779
Glubs9 <37764604+Glubs9@users.noreply.github.com>
2021-08-21 09:06:49 +1000

    added comments about what needs to be added

--- server/transaction.py
+++ server/transaction.py
@@ -6,3 +6,10 @@ class Transaction:
 
 def create_transaction(self, sender, recipient, amount): 
 	return Transaction(sender, recipient, amount)
+
+
+#this file is a mess and we need jamie on it ASAP
+	#cause it's missing blockchain implementation. That is what needs to be in this file
+	#handle local ledgers as well when you change this file
+	
+#don't forget to add functions that keep getting deleted for the server api
